dae40cd50a7c835eddbc6f4235778461bae4ebbc
huynhnhan112 <huunhan557@gmail.com>
2021-06-22 18:29:16 +0700

    DevB vua code

---
+++ controllers/DevA.js
@@ -0,0 +1,10 @@
+console.log('Chức năng này DevA viết !')
+
+function devA () {
+    console.log('code code code')
+}
+
+function createBranchDevA () {
+    console.log('DevA vừa tạo nhánh !')
+}
+
---
+++ controllers/DevB.js
@@ -0,0 +1,9 @@
+onsole.log('Chức năng này DevA viết !')
+
+function devB () {
+    console.log('code code code')
+}
+
+function createBranchDevB () {
+    console.log('DevB vừa tạo nhánh !')
+}
